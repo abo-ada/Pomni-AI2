3b05f65886ff813ee3dc6c00f6ed69aedfe46cfb
abo-ada <abo.adam.hg.0v1@gmail.com>
2026-05-10 01:17:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '201099271157', // Bot number
   prefix: [".", "/", "!"],
-  fromMe: false, 
+  fromMe: null, 
   owners: [
   // Owner 1
-    { name: "VA", lid: "247579682029763@lid", jid: "972569311531@s.whatsapp.net" },
+    { name: "AM", lid: "247579682029763@lid", jid: "972569311531@s.whatsapp.net" },
   // Owner 2
-    { name: "emam", lid: "221307316789354@lid", jid: "201144480436@s.whatsapp.net" },
+    { name: "memo", lid: "221307316789354@lid", jid: "201144480436@s.whatsapp.net" },
   // Owner 3
-    { name: "Sukuna", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" },
+    { name: "Sukuna", jid: "201099271157@s.whatsapp.net", lid: "50414477168824@lid" },
   // Owner 4 
-   { name: "عمورتي", jid: "201050079089@s.whatsapp.net", lid: "51664513925368@lid" }
+   { name: "عمورتي", jid: "201099271157@s.whatsapp.net", lid: "51664513925368@lid" }
   ],
-  settings: { noWelcome: false },
+  settings: { noWelcome: null },
   commandsPath: './plugins'
 });
 
@@ -33,17 +33,18 @@ if (!global.db) {
 /* =========== Config ========== */
 const { config } = client;
 config.info = { 
-  nameBot: "♡ 𝙋𝙊𝙈𝙉𝙄 🎪 〈", 
-  nameChannel: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", 
+  nameBot: "♡ 𝑀𝐴𝑅𝑂 🎪 〈", 
+  nameChannel: "𝑀𝐴𝑅𝑂 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", 
   idChannel: "120363225356834044@newsletter",
   urls: {
     repo: "https://github.com/deveni0/Pomni-AI",
     api: "https://emam-api.web.id",
-    channel: "https://whatsapp.com/channel/0029VaQim2bAu3aPsRVaDq3v"
+    channel: "https://whatsapp.com/channel/0029VbD9c2YKWEKwBYD1Gh2l
+"
   },
   copyright: { 
-    pack: 'ڤـ ـ VA ـ ـا', 
-    author: 'VA'
+    pack: 'ڤـ ـ AM ـ ـا', 
+    author: 'AM'
   },
   images: [
     "https://i.pinimg.com/originals/11/26/97/11269786cdb625c60213212aa66273a9.png",
